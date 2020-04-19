20124f30d214c891ce4379c86646c504cba6d45a
zk <zk>
2020-04-19 19:14:00 +0800

    avplayer初体验

--- ZKDemos/onLineVideo/HelloAvPlayerController.m
+++ ZKDemos/onLineVideo/HelloAvPlayerController.m
@@ -9,11 +9,14 @@
 #import "HelloAvPlayerController.h"
 #import <AVKit/AVKit.h>
 #import "ZKAvPlayerView.h"
+#import "Masonry.h"
 
 @interface HelloAvPlayerController ()
 
 @property (weak, nonatomic) IBOutlet UIView *cusView;
 @property (weak, nonatomic) ZKAvPlayerView * zkPlayerView;
+
+@property (assign, nonatomic) BOOL firstDidAppear;
 
 @end
 
@@ -22,14 +25,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    ZKAvPlayerView *view = [[ZKAvPlayerView alloc] init];
-    self.zkPlayerView = view;
-    [self.cusView addSubview:view];
-    self.zkPlayerView.frame = self.cusView.bounds;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
     
-    [self.zkPlayerView playWithUrl:@"https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4" startPlayBlock:^(CMTime duration) {
-        NSLog(@"%f",CMTimeGetSeconds(duration));
-    }];
+    if (!_firstDidAppear) {
+        _firstDidAppear = YES;
+        
+        ZKAvPlayerView *view = [[ZKAvPlayerView alloc] init];
+        self.zkPlayerView = view;
+        [self.cusView addSubview:view];
+        self.zkPlayerView.frame = self.cusView.bounds;
+        
+        [self.zkPlayerView playWithUrl:@"https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4" startPlayBlock:^(CMTime duration) {
+            NSLog(@"%f",CMTimeGetSeconds(duration));
+        }];
+    }
 }
 
 @end
